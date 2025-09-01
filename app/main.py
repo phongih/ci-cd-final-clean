f7039069ca61f9aabaa7fff6824695c9d8bd2ed0
Peter Tran Quoc Phong <phongtq@sna.edu.vn>
2025-09-01 15:47:04 +0700

    Initial clean CI/CD project

---
+++ app/main.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    """Return sum of two numbers."""
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """Return difference of two numbers."""
+    return a - b
---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from app import main
+
+
+def test_add():
+    assert main.add(2, 3) == 5
+
+
+def test_subtract():
+    assert main.subtract(5, 2) == 3
